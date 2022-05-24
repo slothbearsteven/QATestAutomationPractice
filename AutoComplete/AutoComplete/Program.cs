c8f40f7cfdfa3512ba0060b4995e653c6e6721d1
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-24 16:38:27 -0600

    Finished autocomplete practice with selenium

---
+++ AutoComplete/AutoComplete/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/autocomplete";
+
+            IWebElement autocomplete = driver.FindElement(By.Id("autocomplete"));
+            autocomplete.SendKeys("123 sesame");
+            Thread.Sleep(1000);
+
+            //due to the maps not loading correctly, targeted the dismissbutton instead
+            IWebElement autocompleteResult = driver.FindElement(By.ClassName("dismissButton"));
+            autocompleteResult.Click();
+
+          
+
+
+            driver.Quit();
+
+        }
+    }
+}
