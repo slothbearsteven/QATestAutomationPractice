dc144d689e2a236fd3f17c2a7ae7bfd2871fb663
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-24 15:57:15 -0600

    Initial commit

---
+++ KeyAndMousePress/KeyAndMousePress/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/keypress";
+            IWebElement nameField = driver.FindElement(By.Id("name"));
+            nameField.Click();
+            nameField.SendKeys("General Kenobi");
+            IWebElement button = driver.FindElement(By.Id("button"));
+            button.Click();
+            driver.Quit();
+
+        }
+    }
+}
