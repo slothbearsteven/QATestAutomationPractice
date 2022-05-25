5bbc5737df409f8845078f7dbf60f253fd5f5604
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-24 21:13:03 -0600

    Switch Window Test done

---
+++ SwitchWindow/SwitchWindow/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/switch-window";
+
+            IWebElement newTabButton = driver.FindElement(By.Id("new-tab-button"));
+            newTabButton.Click();
+
+            string originalHandle = driver.CurrentWindowHandle;
+
+            List<string> lstWindow = driver.WindowHandles.ToList();
+            driver.SwitchTo().Window(lstWindow[1]);
+            driver.SwitchTo().Window(originalHandle);
+
+
+
+            driver.Quit();
+
+        }
+    }
+}
