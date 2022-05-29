450d24730d1a8d33527c37b38047fbec90d56a77
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-29 15:40:46 -0600

    alert window test written with success

---
+++ alertWindow/alertWindow/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/switch-window";
+
+            IWebElement alertButton = driver.FindElement(By.Id("alert-button"));
+            alertButton.Click();
+
+             IAlert alertBox =driver.SwitchTo().Alert();
+            alertBox.Accept();
+
+
+
+
+            driver.Quit();
+
+        }
+    }
+}
