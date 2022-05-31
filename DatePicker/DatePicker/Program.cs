1399802b4be1ad25e3afb5bd889a8a14f69f387b
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-31 15:25:09 -0600

    date picker page done

---
+++ DatePicker/DatePicker/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/datepicker";
+
+            IWebElement dateField = driver.FindElement(By.Id("datepicker"));
+            dateField.Click();
+            dateField.SendKeys("08/29/2023");
+            dateField.SendKeys(Keys.Return);
+
+
+            driver.Quit();
+
+        }
+    }
+}
