3d2adbe40a669adb7dceb721e7c30e1a6854c7b0
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-31 13:12:36 -0600

    Drag and drop finished, finished advance selectors in course

---
+++ DragAndDrop/DragAndDrop/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/dragdrop";
+
+            IWebElement image = driver.FindElement(By.Id("image"));
+            IWebElement dropBox = driver.FindElement(By.Id("box"));
+
+            Actions actions = new Actions(driver);
+            actions.DragAndDrop(image, dropBox).Build().Perform();
+
+            driver.Quit();
+
+        }
+    }
+}
