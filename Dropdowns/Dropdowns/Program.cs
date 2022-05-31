b90430a48916e185b70df73801534f3dcacf0e26
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-31 15:47:30 -0600

    dropdowns finished

---
+++ Dropdowns/Dropdowns/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/dropdown";
+
+            IWebElement dropDownButton = driver.FindElement(By.Id("dropdownMenuButton"));
+            dropDownButton.Click();
+
+            IWebElement dropDownSelection = driver.FindElement(By.XPath("/html/body/div/div/div/a[6]"));
+            dropDownSelection.Click();
+
+       
+
+
+            driver.Quit();
+
+        }
+    }
+}
