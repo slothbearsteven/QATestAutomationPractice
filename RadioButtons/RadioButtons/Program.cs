4ab30059584f69d3f05cddd4c9b5ea8954f5d95a
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-05-31 14:26:40 -0600

    Radio Button Page finished

---
+++ RadioButtons/RadioButtons/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace Selenium
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://formy-project.herokuapp.com/radiobutton";
+
+            IWebElement radioButton1 = driver.FindElement(By.Id("radio-button-1"));
+            radioButton1.Click();
+
+            IWebElement radioButton2 = driver.FindElement(By.CssSelector("input[value='option2']"));
+            radioButton2.Click();
+
+            IWebElement radioButton3 = driver.FindElement(By.XPath("/html/body/div/div[3]/input"));
+            radioButton3.Click();
+
+            driver.Quit();
+
+        }
+    }
+}
