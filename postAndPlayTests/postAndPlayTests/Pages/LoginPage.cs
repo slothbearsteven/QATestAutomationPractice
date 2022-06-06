de737816dfc33c556b64dc8f885f4da343e763d8
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-06-06 14:39:00 -0600

    started post and  play tests

---
+++ postAndPlayTests/postAndPlayTests/Pages/LoginPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postAndPlayTests.Pages
+{
+    internal class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            this.driver = webDriver;
+            PageFactory.InitElements(driver, this);
+        }
+
+    }
+}
